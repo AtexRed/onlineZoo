7a0c5994715361bd60db9a41955fff4ce45fc0dd
AtexRed <61019488+AtexRed@users.noreply.github.com>
2021-04-05 18:00:31 +0300

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,29 @@
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var modalButtons = document.querySelectorAll('.map__animal-icon'),
+        overlay      = document.querySelector('#overlay-modal'),
+        closeButtons = document.querySelectorAll('.modal__close');
+
+    modalButtons.forEach(function(item){
+        item.addEventListener('click', function(e) {
+            var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+             modalElem.classList.add('active');
+             overlay.classList.add('active');
+             e.preventDefault();
+        });
+    });
+
+    closeButtons.forEach(function(item) {
+        item.addEventListener('click', function(e) {
+            var parentModal = this.closest('.modal');
+            parentModal.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+    });
+     overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+});
